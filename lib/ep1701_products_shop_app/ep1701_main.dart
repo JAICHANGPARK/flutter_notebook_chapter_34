f7062fef51e6e74e3c0a1b8e0d50d68ba8f5d03b
JAICHANGPARK <aristojeff@gmail.com>
2023-11-16 19:35:03 +0900

    Update ep1701_main.dart

--- lib/ep1701_products_shop_app/ep1701_main.dart
+++ lib/ep1701_products_shop_app/ep1701_main.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_notebook_chapter_34/ep1701_products_shop_app/views/product_main_page.dart';
 
 void main() {
-
+  runApp(ProductMainPage());
 }
 
 class ProductsShopApp extends StatelessWidget {
