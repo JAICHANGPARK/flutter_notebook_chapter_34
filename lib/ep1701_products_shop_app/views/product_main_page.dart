e7c22303b474507cd61f2038d156986f804bdec2
JAICHANGPARK <aristojeff@gmail.com>
2023-11-16 19:40:00 +0900

    Update product_main_page.dart

--- lib/ep1701_products_shop_app/views/product_main_page.dart
+++ lib/ep1701_products_shop_app/views/product_main_page.dart
@@ -11,7 +11,38 @@ class _ProductMainPageState extends State<ProductMainPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Column(),
+      body: SafeArea(
+        child: Column(
+          children: [
+            Row(
+              children: [
+                Expanded(
+                  child: Container(
+                    decoration: BoxDecoration(
+                      color: Colors.grey[300],
+                      borderRadius: BorderRadius.circular(8),
+                    ),
+                    child: TextField(
+                      controller: TextEditingController(),
+                      decoration: InputDecoration(),
+                    ),
+                  ),
+                ),
+                Container(
+                  decoration: BoxDecoration(
+                    color: Colors.grey[300],
+                    borderRadius: BorderRadius.circular(8),
+                  ),
+                  child: IconButton(
+                    icon: Icon(Icons.percent),
+                    onPressed: () {},
+                  ),
+                ),
+              ],
+            )
+          ],
+        ),
+      ),
     );
   }
 }
