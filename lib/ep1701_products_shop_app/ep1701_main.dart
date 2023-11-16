698189f6e532dc9005a90e49f479a7e679c1a705
JAICHANGPARK <aristojeff@gmail.com>
2023-11-16 19:31:55 +0900

    Create ep1701_main.dart

---
+++ lib/ep1701_products_shop_app/ep1701_main.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  
+}
+
+class ProductsShopApp extends StatelessWidget {
+  const ProductsShopApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
