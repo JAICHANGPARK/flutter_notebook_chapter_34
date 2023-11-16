ffcec5cbb0a3cfbc3c803f9a1e59716b1f42855b
JAICHANGPARK <aristojeff@gmail.com>
2023-11-16 19:33:46 +0900

    Create product_main_page.dart

---
+++ lib/ep1701_products_shop_app/views/product_main_page.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+
+class ProductMainPage extends StatefulWidget {
+  const ProductMainPage({super.key});
+
+  @override
+  State<ProductMainPage> createState() => _ProductMainPageState();
+}
+
+class _ProductMainPageState extends State<ProductMainPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
