4abb04f8499de7a9f7f21584c0d5051182ae115b
JAICHANGPARK <aristojeff@gmail.com>
2023-11-17 20:29:30 +0900

    Update product_main_page.dart

--- lib/ep1701_products_shop_app/views/product_main_page.dart
+++ lib/ep1701_products_shop_app/views/product_main_page.dart
@@ -162,7 +162,10 @@ class _ProductMainPageState extends State<ProductMainPage> {
                 children: [
                   Text("Exclusive"),
                   Container(
-                    decoration: BoxDecoration(color: Colors.tealAccent, borderRadius: BorderRadius.circular(16)),
+                    decoration: BoxDecoration(
+                      color: Colors.tealAccent,
+                      borderRadius: BorderRadius.circular(16),
+                    ),
                     child: Text("03:25:43"),
                   )
                 ],
