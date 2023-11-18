132f1464118fafc77246437bee02fd4d18d9d449
JAICHANGPARK <aristojeff@gmail.com>
2023-11-18 10:31:09 +0900

    Update product_detail_page.dart

--- lib/ep1701_products_shop_app/views/product_detail_page.dart
+++ lib/ep1701_products_shop_app/views/product_detail_page.dart
@@ -19,6 +19,59 @@ class _ProductDetailPageState extends State<ProductDetailPage> {
             decoration: BoxDecoration(
               color: Colors.white,
             ),
+            child: Column(
+              children: [
+                Row(
+                  children: [
+                    CircleAvatar(),
+                    Column(
+                      children: [
+                        Text("Design house"),
+                        Row(
+                          children: [
+                            Icon(
+                              Icons.star,
+                              color: Colors.purpleAccent,
+                            ),
+                            Text('4.9'),
+                            Text("(127)"),
+                          ],
+                        ),
+                      ],
+                    ),
+                    Container(
+                      decoration: BoxDecoration(
+                        color: Colors.black,
+                      ),
+                      child: Icon(Icons.chat_bubble_outline),
+                    ),
+                  ],
+                ),
+                Row(
+                  children: [
+                    Text("Lamp"),
+                    Text("Posted 1 hr ago"),
+                  ],
+                ),
+                Row(
+                  children: [
+                    Container(
+                      decoration: BoxDecoration(
+                        color: Colors.tealAccent,
+                      ),
+                      child: Row(
+                        children: [
+                          Icon(
+                            Icons.refresh,
+                          ),
+                          Text("Buy Online"),
+                        ],
+                      ),
+                    )
+                  ],
+                )
+              ],
+            ),
           )
         ],
       ),
