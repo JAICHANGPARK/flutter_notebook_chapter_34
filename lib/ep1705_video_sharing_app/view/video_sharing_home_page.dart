e9a859d61218b2186c3b65e5dc5fd5d72a5358c0
JAICHANGPARK <aristojeff@gmail.com>
2023-11-20 17:30:40 +0900

    Update video_sharing_home_page.dart

--- lib/ep1705_video_sharing_app/view/video_sharing_home_page.dart
+++ lib/ep1705_video_sharing_app/view/video_sharing_home_page.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 
-
 class VideoSharingHomePage extends StatefulWidget {
   const VideoSharingHomePage({super.key});
 
@@ -11,6 +10,23 @@ class VideoSharingHomePage extends StatefulWidget {
 class _VideoSharingHomePageState extends State<VideoSharingHomePage> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      backgroundColor: Colors.black,
+      body: SafeArea(
+        child: Column(
+          children: [
+            Row(
+              children: [
+                CircleAvatar(
+                  backgroundColor: Colors.white.withOpacity(0.1),
+                  child: Icon(Icons.search),
+                  foregroundColor: Colors.white,
+                )
+              ],
+            )
+          ],
+        ),
+      ),
+    );
   }
 }
