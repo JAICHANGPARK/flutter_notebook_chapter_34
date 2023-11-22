248f8f14c1b9051d03c56f6461b5a2776f8f00db
JAICHANGPARK <aristojeff@gmail.com>
2023-11-22 18:30:20 +0900

    Update video_streaming_page.dart

--- lib/ep1705_video_sharing_app/view/video_streaming_page.dart
+++ lib/ep1705_video_sharing_app/view/video_streaming_page.dart
@@ -13,7 +13,18 @@ class _VideoSharingLivePageState extends State<VideoSharingLivePage> {
     return Scaffold(
       body: Stack(
         children: [
-
+          Positioned.fill(
+            child: Image.network(
+              "https://cdn.pixabay.com/photo/2021/03/02/01/07/cyberpunk-6061251_1280.jpg",
+              fit: BoxFit.cover,
+            ),
+          ),
+          Positioned(
+            left: 0,
+            right: 0,
+            bottom: 0,
+            child: Container(),
+          )
         ],
       ),
     );
