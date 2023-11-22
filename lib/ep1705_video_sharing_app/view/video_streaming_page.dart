544e4ee595260996b1edef6063d7dcce5331cde2
JAICHANGPARK <aristojeff@gmail.com>
2023-11-22 18:25:41 +0900

    Create video_streaming_page.dart

---
+++ lib/ep1705_video_sharing_app/view/video_streaming_page.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+
+class VideoSharingLivePage extends StatefulWidget {
+  const VideoSharingLivePage({super.key});
+
+  @override
+  State<VideoSharingLivePage> createState() => _VideoSharingLivePageState();
+}
+
+class _VideoSharingLivePageState extends State<VideoSharingLivePage> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
