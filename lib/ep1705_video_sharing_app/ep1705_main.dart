a932d0f72af67f618dcd7895afb1fbddb03fd8d1
Dreamwalker <aristojeff@gmail.com>
2023-11-23 11:20:21 +0900

    Update ep1705_main.dart

--- lib/ep1705_video_sharing_app/ep1705_main.dart
+++ lib/ep1705_video_sharing_app/ep1705_main.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_notebook_chapter_34/ep1705_video_sharing_app/view/video_sharing_home_page.dart';
 
 void main() {
-  runApp(VideoSharingApp());
+  runApp(const VideoSharingApp());
 }
 
 class VideoSharingApp extends StatelessWidget {
@@ -10,7 +10,7 @@ class VideoSharingApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return const MaterialApp(
       home: VideoSharingHomePage(),
     );
   }
