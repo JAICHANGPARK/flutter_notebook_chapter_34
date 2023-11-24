67e0a401f9093892bf5675d2bf93144a04565d90
JAICHANGPARK <aristojeff@gmail.com>
2023-11-24 18:11:55 +0900

    Update yoga_coaching_detail_page.dart

--- lib/ep1709_yoga_coaching_app/views/yoga_coaching_detail_page.dart
+++ lib/ep1709_yoga_coaching_app/views/yoga_coaching_detail_page.dart
@@ -29,6 +29,7 @@ class _YogaCoachingDetailPageState extends State<YogaCoachingDetailPage> {
       body: Padding(
         padding: const EdgeInsets.all(16.0),
         child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
           children: [
             Text(
               "Schedule data",
