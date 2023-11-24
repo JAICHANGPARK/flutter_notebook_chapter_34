c0dd7ce92b3df54063df86249fb63e274e2d1d12
JAICHANGPARK <aristojeff@gmail.com>
2023-11-24 17:33:16 +0900

    Update yoga_coaching_home_page.dart

--- lib/ep1709_yoga_coaching_app/views/yoga_coaching_home_page.dart
+++ lib/ep1709_yoga_coaching_app/views/yoga_coaching_home_page.dart
@@ -27,7 +27,35 @@ class _YogaCoachingHomePageState extends State<YogaCoachingHomePage> {
       ),
       body: SafeArea(
         child: Column(
-          children: [],
+          children: [
+            Container(
+              height: 64,
+              color: Colors.blue,
+              child: Row(
+                children: [
+                  Expanded(
+                    child: Stack(
+                      children: [
+                        Positioned(
+                          left: 0,
+                          right: 0,
+                          bottom: 0,
+                          top: 0,
+                          child: Container(
+                            height: 24,
+                            color: Colors.black,
+                          ),
+                        )
+                      ],
+                    ),
+                  ),
+                  Expanded(
+                    child: Stack(),
+                  ),
+                ],
+              ),
+            )
+          ],
         ),
       ),
     );
