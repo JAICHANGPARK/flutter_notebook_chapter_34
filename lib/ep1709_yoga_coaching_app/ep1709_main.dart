9062be941e7b59f53b5c284722c49afee6cba875
JAICHANGPARK <aristojeff@gmail.com>
2023-11-24 17:27:22 +0900

    Create ep1709_main.dart

---
+++ lib/ep1709_yoga_coaching_app/ep1709_main.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(YogaCoachingApp());
+
+class YogaCoachingApp extends StatelessWidget {
+  const YogaCoachingApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp();
+  }
+}
