2612946a3c149a1150f3ef555eeb2f49f54d93d0
Dreamwalker <aristojeff@gmail.com>
2023-11-25 07:03:41 +0900

    Update ep1709_main.dart

--- lib/ep1709_yoga_coaching_app/ep1709_main.dart
+++ lib/ep1709_yoga_coaching_app/ep1709_main.dart
@@ -1,14 +1,14 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_notebook_chapter_34/ep1709_yoga_coaching_app/views/yoga_coaching_home_page.dart';
 
-void main() => runApp(YogaCoachingApp());
+void main() => runApp(const YogaCoachingApp());
 
 class YogaCoachingApp extends StatelessWidget {
   const YogaCoachingApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return const MaterialApp(
       home: YogaCoachingHomePage(),
     );
   }
