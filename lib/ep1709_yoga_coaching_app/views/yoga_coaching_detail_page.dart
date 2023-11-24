92a1ba8db03a46c2f4d573732150d862a9be953a
JAICHANGPARK <aristojeff@gmail.com>
2023-11-24 18:06:39 +0900

    Create yoga_coaching_detail_page.dart

---
+++ lib/ep1709_yoga_coaching_app/views/yoga_coaching_detail_page.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class YogaCoachingDetailPage extends StatefulWidget {
+  const YogaCoachingDetailPage({super.key});
+
+  @override
+  State<YogaCoachingDetailPage> createState() => _YogaCoachingDetailPageState();
+}
+
+class _YogaCoachingDetailPageState extends State<YogaCoachingDetailPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
