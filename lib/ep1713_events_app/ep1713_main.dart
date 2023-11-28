3d6ca5e525c75243bb34fe8b826f94541dc47208
JAICHANGPARK <aristojeff@gmail.com>
2023-11-28 18:57:55 +0900

    Create ep1713_main.dart

---
+++ lib/ep1713_events_app/ep1713_main.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  
+}
+
+class EventsApp extends StatelessWidget {
+  const EventsApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
