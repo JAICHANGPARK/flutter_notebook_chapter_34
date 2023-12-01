2c7df030c0d520c92d72dc4104616d650cbb3634
JAICHANGPARK <aristojeff@gmail.com>
2023-12-01 18:18:32 +0900

    Update renting_home_page.dart

--- lib/ep1717_renting_app/view/renting_home_page.dart
+++ lib/ep1717_renting_app/view/renting_home_page.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 
-
 class RentingHomePage extends StatefulWidget {
   const RentingHomePage({super.key});
 
@@ -11,6 +10,40 @@ class RentingHomePage extends StatefulWidget {
 class _RentingHomePageState extends State<RentingHomePage> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      backgroundColor: Color(0xff151617),
+      body: SafeArea(
+        child: Column(
+          children: [
+            Row(
+              children: [
+                IconButton(
+                  onPressed: () {},
+                  icon: Icon(
+                    Icons.search,
+                    color: Colors.white,
+                  ),
+                ),
+                Expanded(
+                  child: Container(
+                    decoration: BoxDecoration(
+                      color: Colors.white.withOpacity(0.4),
+                    ),
+                  ),
+                ),
+                Badge(
+                  child: Icon(
+                    Icons.search,
+                    color: Colors.white,
+                  ),
+                  label: Text(""),
+                  
+                ),
+              ],
+            )
+          ],
+        ),
+      ),
+    );
   }
 }
