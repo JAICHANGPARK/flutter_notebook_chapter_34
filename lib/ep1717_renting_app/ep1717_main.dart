d353816650c68b859360499fbea7275dc88c0361
JAICHANGPARK <aristojeff@gmail.com>
2023-12-01 18:16:09 +0900

    Update ep1717_main.dart

--- lib/ep1717_renting_app/ep1717_main.dart
+++ lib/ep1717_renting_app/ep1717_main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_notebook_chapter_34/ep1717_renting_app/view/renting_home_page.dart';
 
 void main() {
   runApp(RentingApp());
@@ -10,7 +11,7 @@ class RentingApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-
+      home: RentingHomePage(),
     );
   }
 }
