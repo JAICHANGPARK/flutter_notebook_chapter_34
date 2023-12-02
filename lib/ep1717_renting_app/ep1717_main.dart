9d9e1141248220c1542f67d689969e3c86dd8716
Dreamwalker <aristojeff@gmail.com>
2023-12-02 18:47:24 +0900

    ep1717_main.dart 업데이트

--- lib/ep1717_renting_app/ep1717_main.dart
+++ lib/ep1717_renting_app/ep1717_main.dart
@@ -1,9 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_notebook_chapter_34/ep1717_renting_app/view/renting_home_page.dart';
 
-void main() {
-  runApp(RentingApp());
-}
+void main() => runApp(RentingApp());
+
 
 class RentingApp extends StatelessWidget {
   const RentingApp({super.key});
