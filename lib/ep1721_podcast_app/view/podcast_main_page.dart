bb16125ec12a6c83a4d180c2e2248d82041fd10d
JAICHANGPARK <aristojeff@gmail.com>
2023-12-06 18:58:30 +0900

    Update podcast_main_page.dart

--- lib/ep1721_podcast_app/view/podcast_main_page.dart
+++ lib/ep1721_podcast_app/view/podcast_main_page.dart
@@ -10,6 +10,6 @@ class PodcastMainPage extends StatefulWidget {
 class _PodcastMainPageState extends State<PodcastMainPage> {
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold();
   }
 }
