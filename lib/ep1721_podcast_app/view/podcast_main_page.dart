cea9da4629fb18e86c185e148923a6a1b96533df
JAICHANGPARK <aristojeff@gmail.com>
2023-12-06 19:10:09 +0900

    Update podcast_main_page.dart

--- lib/ep1721_podcast_app/view/podcast_main_page.dart
+++ lib/ep1721_podcast_app/view/podcast_main_page.dart
@@ -24,77 +24,80 @@ class _PodcastMainPageState extends State<PodcastMainPage> {
                 child: Row(
                   children: [
                     Expanded(
-                      flex: 6,
+                        flex: 6,
                         child: Row(
-                      children: [
-                        Expanded(
-                          child: Stack(
-                            children: [
-                              Positioned(
-                                left: 8,
-                                right: 8,
-                                top: 0,
-                                bottom: 0,
-                                child: Center(
-                                  child: Container(
-                                    height: 18,
-                                    color: Colors.black,
-                                  ),
-                                ),
-                              ),
-                              Positioned(
-                                left: 0,
-                                top: 0,
-                                bottom: 0,
-                                right: 0,
-                                child: Row(
-                                  children: [
-                                    CircleAvatar(
-                                      radius: 24,
-                                      backgroundColor: Colors.black,
-                                    ),
-                                    Gap(5),
-                                    Expanded(
+                          children: [
+                            Expanded(
+                              child: Stack(
+                                children: [
+                                  Positioned(
+                                    left: 8,
+                                    right: 8,
+                                    top: 0,
+                                    bottom: 0,
+                                    child: Center(
                                       child: Container(
-                                        decoration: BoxDecoration(
-                                          color: Colors.black,
-                                          borderRadius: BorderRadius.circular(32),
-                                        ),
-                                        padding: EdgeInsets.symmetric(horizontal: 32, vertical: 8),
-                                        child: GridView.builder(
-                                          gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-                                              crossAxisCount: 10, crossAxisSpacing: 2, mainAxisSpacing: 2),
-                                          itemBuilder: (context, index) {
-                                            return Container(
-                                              color: index % 6 == 0?  Colors.black :Colors.white,
-                                            );
-                                          },
-                                        ),
+                                        height: 18,
+                                        color: Colors.black,
                                       ),
-                                    )
-                                  ],
-                                ),
+                                    ),
+                                  ),
+                                  Positioned(
+                                    left: 0,
+                                    top: 0,
+                                    bottom: 0,
+                                    right: 0,
+                                    child: Row(
+                                      children: [
+                                        CircleAvatar(
+                                          radius: 24,
+                                          backgroundColor: Colors.black,
+                                        ),
+                                        Gap(5),
+                                        Expanded(
+                                          child: Container(
+                                            decoration: BoxDecoration(
+                                              color: Colors.black,
+                                              borderRadius: BorderRadius.circular(32),
+                                            ),
+                                            padding: EdgeInsets.symmetric(horizontal: 32, vertical: 8),
+                                            child: GridView.builder(
+                                              gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                                                crossAxisCount: 10,
+                                                crossAxisSpacing: 2,
+                                                mainAxisSpacing: 2,
+                                              ),
+                                              itemBuilder: (context, index) {
+                                                return Container(
+                                                  color: index % 6 == 0 ? Colors.black : Colors.white,
+                                                );
+                                              },
+                                            ),
+                                          ),
+                                        )
+                                      ],
+                                    ),
+                                  ),
+                                ],
                               ),
-                            ],
-                          ),
-                        ),
-                        IconButton(
-                          onPressed: () {},
-                          icon: Icon(Icons.keyboard_arrow_down),
-                        )
-                      ],
-                    )),
+                            ),
+                            IconButton(
+                              onPressed: () {},
+                              icon: Icon(Icons.keyboard_arrow_down),
+                            )
+                          ],
+                        )),
                     Expanded(
-                      flex: 4,
+                        flex: 4,
                         child: Row(
-                      children: [
-                        Spacer(),
-                        IconButton(
-                          onPressed: () {},
-                          icon: Icon(Icons.search),
-                        )
-                      ],
-                    ))
+                          children: [
+                            Spacer(),
+                            IconButton(
+                              onPressed: () {},
+                              icon: Icon(Icons.search),
+                            )
+                          ],
+                        ))
                   ],
                 ),
               ),
