bbb9a697c6e6a6c16fea7192653defde5f2f0339
JAICHANGPARK <aristojeff@gmail.com>
2023-12-06 18:54:59 +0900

    Update ep1721_main.dart

--- lib/ep1721_podcast_app/ep1721_main.dart
+++ lib/ep1721_podcast_app/ep1721_main.dart
@@ -1,10 +1,17 @@
 import 'package:flutter/material.dart';
 
+void main() {
+  runApp(PodCastApp());
+}
+
 class PodCastApp extends StatelessWidget {
   const PodCastApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return MaterialApp(
+      
+    );
+
   }
 }
