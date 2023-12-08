2da7b32cd3fb890751fb1aa0088b1a5cd7cbf31f
JAICHANGPARK <aristojeff@gmail.com>
2023-12-08 17:26:15 +0900

    Create real_estate_login_page.dart

---
+++ lib/ep1724_real_estate_app/views/real_estate_login_page.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+
+class RealEstateLoginPage extends StatefulWidget {
+  const RealEstateLoginPage({super.key});
+
+  @override
+  State<RealEstateLoginPage> createState() => _RealEstateLoginPageState();
+}
+
+class _RealEstateLoginPageState extends State<RealEstateLoginPage> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
