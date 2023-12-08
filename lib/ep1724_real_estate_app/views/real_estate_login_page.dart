c985e319b8f36153fceb87803cd5c3136a3083e2
JAICHANGPARK <aristojeff@gmail.com>
2023-12-08 17:26:58 +0900

    Update real_estate_login_page.dart

--- lib/ep1724_real_estate_app/views/real_estate_login_page.dart
+++ lib/ep1724_real_estate_app/views/real_estate_login_page.dart
@@ -11,6 +11,6 @@ class RealEstateLoginPage extends StatefulWidget {
 class _RealEstateLoginPageState extends State<RealEstateLoginPage> {
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold();
   }
 }
