58a020ced316de592c93054ae0be13aff1865eb8
JAICHANGPARK <aristojeff@gmail.com>
2023-12-13 21:07:37 +0900

    Update sport_start_page.dart

--- lib/ep1731_sports_app/view/sport_start_page.dart
+++ lib/ep1731_sports_app/view/sport_start_page.dart
@@ -12,12 +12,22 @@ class _SportsStartPageState extends State<SportsStartPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: SafeArea(
-        child: Column(
-          children: [
-            
-          ],
-        ),
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Expanded(child: Container()),
+          Container(
+              color: Colors.black,
+              padding: EdgeInsets.symmetric(vertical: 42, horizontal: 16),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text("YOUR FITNESS COACH"),
+                  Text("Welcome to Your Ultimate Training Companion",)
+                ],
+              ))
+
+        ],
       ),
     );
   }
