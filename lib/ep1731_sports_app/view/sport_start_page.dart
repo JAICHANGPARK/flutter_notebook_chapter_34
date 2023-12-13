e87b7627c1e6e7b1eaf0abaaa32e2cdb9ca0e18f
JAICHANGPARK <aristojeff@gmail.com>
2023-12-13 21:11:22 +0900

    Update sport_start_page.dart

--- lib/ep1731_sports_app/view/sport_start_page.dart
+++ lib/ep1731_sports_app/view/sport_start_page.dart
@@ -17,46 +17,48 @@ class _SportsStartPageState extends State<SportsStartPage> {
         children: [
           Expanded(child: Container()),
           Container(
-              width: double.infinity,
-              color: Colors.black,
-              padding: EdgeInsets.symmetric(vertical: 42, horizontal: 16),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Gap(24),
-                  Text(
-                    "YOUR FITNESS COACH",
-                    style: TextStyle(color: Colors.tealAccent),
+            width: double.infinity,
+            color: Colors.black,
+            padding: EdgeInsets.symmetric(vertical: 42, horizontal: 16),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Gap(24),
+                Text(
+                  "YOUR FITNESS COACH",
+                  style: TextStyle(color: Colors.tealAccent),
+                ),
+                Gap(24),
+                Text(
+                  "Welcome to Your\nUltimate Training\nCompanion",
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 32,
+                    fontWeight: FontWeight.bold,
                   ),
-                  Gap(24),
-                  Text(
-                    "Welcome to Your\nUltimate Training\nCompanion",
-                    style: TextStyle(
-                      color: Colors.white,
-                      fontSize: 32,
-                      fontWeight: FontWeight.bold,
-                    ),
+                ),
+                Gap(24),
+                Container(
+                  padding: EdgeInsets.symmetric(vertical: 16),
+                  decoration: BoxDecoration(
+                    color: Colors.tealAccent,
                   ),
-                  Gap(24),
-                  Container(
-                    padding: EdgeInsets.symmetric(vertical: 16),
-                    decoration: BoxDecoration(
-                      color: Colors.tealAccent,
-                    ),
-                    child: Row(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: [
-                        Text(
-                          "GET STARTED",
-                        ),
-                        Icon(
-                          Icons.play_arrow,
-                        )
-                      ],
-                    ),
-                  )
-                ],
-              ))
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Text(
+                        "GET STARTED",
+                      ),
+                      Icon(
+                        Icons.play_arrow,
+                      )
+                    ],
+                  ),
+                ),
+                Gap(24),
+              ],
+            ),
+          ),
         ],
       ),
     );
