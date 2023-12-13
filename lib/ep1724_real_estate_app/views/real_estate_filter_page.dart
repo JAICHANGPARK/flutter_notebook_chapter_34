957a03cf69514ea9dff4f8537b1e6505489e9e98
JAICHANGPARK <aristojeff@gmail.com>
2023-12-13 19:27:44 +0900

    Create real_estate_filter_page.dart

---
+++ lib/ep1724_real_estate_app/views/real_estate_filter_page.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+
+
+class RealEstateFilterPage extends StatefulWidget {
+  const RealEstateFilterPage({super.key});
+
+  @override
+  State<RealEstateFilterPage> createState() => _RealEstateFilterPageState();
+}
+
+class _RealEstateFilterPageState extends State<RealEstateFilterPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+        child: Column(
+          children: [
+            Row(
+              children: [
+                Container(
+                  decoration: BoxDecoration(
+                    shape: BoxShape.circle,
+                    border: Border.all(),
+
+                  ),
+                )
+              ],
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
