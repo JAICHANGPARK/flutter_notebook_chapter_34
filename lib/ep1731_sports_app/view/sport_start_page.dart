81dfb3ba3a5b27ecda735fce8965ea05c935acde
JAICHANGPARK <aristojeff@gmail.com>
2023-12-13 21:05:27 +0900

    Create sport_start_page.dart

---
+++ lib/ep1731_sports_app/view/sport_start_page.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+
+class SportsStartPage extends StatefulWidget {
+  const SportsStartPage({super.key});
+
+  @override
+  State<SportsStartPage> createState() => _SportsStartPageState();
+}
+
+class _SportsStartPageState extends State<SportsStartPage> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
