900581fd122b96b76592d5b2ffa97b0e41e089c2
JAICHANGPARK <aristojeff@gmail.com>
2023-12-13 21:42:20 +0900

    Update sport_app_main_page.dart

--- lib/ep1731_sports_app/view/sport_app_main_page.dart
+++ lib/ep1731_sports_app/view/sport_app_main_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:gap/gap.dart';
 
 class SportsAppMainPage extends StatefulWidget {
   const SportsAppMainPage({super.key});
@@ -11,25 +12,66 @@ class _SportsAppMainPageState extends State<SportsAppMainPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.white,
+      backgroundColor: Colors.black,
       body: Stack(
         children: [
           Positioned.fill(
-            child: Column(
-              children: [
-                Row(
-                  children: [
-                    Text(
-                      "PROGRESS",
-                      style: TextStyle(
-                        fontWeight: FontWeight.bold,
-                        color: Colors.white,
-                        fontSize: 24
+            left: 16,
+            right: 16,
+            top: 24,
+            child: SafeArea(
+              child: Column(
+                children: [
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Text(
+                        "PROGRESS",
+                        style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                          color: Colors.white,
+                          fontSize: 24,
+                        ),
                       ),
-                    )
-                  ],
-                )
-              ],
+                      CircleAvatar(),
+                    ],
+                  ),
+                  Gap(8),
+                  Divider(),
+                  Gap(8),
+                  Row(
+                    children: [
+                      Expanded(
+                        child: Text(
+                          "15,160",
+                          style: TextStyle(
+                            color: Colors.white,
+                            fontWeight: FontWeight.bold,
+                            fontSize: 24,
+                          ),
+                        ),
+                      ),
+                      Column(
+                        crossAxisAlignment: CrossAxisAlignment.end,
+                        children: [
+                          Text(
+                            "Today's GOAL".toUpperCase(),
+                            style: TextStyle(
+                              color: Colors.white,
+                            ),
+                          ),
+                          Text(
+                            "6,000",
+                            style: TextStyle(
+                              color: Colors.tealAccent,
+                            ),
+                          )
+                        ],
+                      )
+                    ],
+                  )
+                ],
+              ),
             ),
           ),
           Positioned(
