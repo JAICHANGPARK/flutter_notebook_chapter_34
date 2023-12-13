b97947fcb830fd72f8c395b2e3ffd1e545d31240
JAICHANGPARK <aristojeff@gmail.com>
2023-12-13 21:04:10 +0900

    Create ep1731_main.dart

---
+++ lib/ep1731_sports_app/ep1731_main.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(SportsApp());
+}
+
+class SportsApp extends StatelessWidget {
+  const SportsApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
