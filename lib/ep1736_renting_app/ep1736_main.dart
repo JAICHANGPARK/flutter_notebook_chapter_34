8029bb612ed19e11bca7fe86d52c3529435679a7
JAICHANGPARK <aristojeff@gmail.com>
2023-12-21 18:50:53 +0900

    Create ep1736_main.dart

---
+++ lib/ep1736_renting_app/ep1736_main.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(RentingApp());
+}
+
+class RentingApp extends StatelessWidget {
+  const RentingApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
