06b8735923b095e66e008346da32690d821939d3
JAICHANGPARK <aristojeff@gmail.com>
2023-12-23 13:07:33 +0900

    Update renting_detail_page.dart

---
+++ lib/ep1736_renting_app/views/renting_detail_page.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+
+class RentingDetailPage extends StatefulWidget {
+  const RentingDetailPage({super.key});
+
+  @override
+  State<RentingDetailPage> createState() => _RentingDetailPageState();
+}
+
+class _RentingDetailPageState extends State<RentingDetailPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(
+        children: [
+          Expanded(child: Placeholder()),
+          Expanded(child: Placeholder()),
+        ],
+      ),
+    );
+  }
+}
