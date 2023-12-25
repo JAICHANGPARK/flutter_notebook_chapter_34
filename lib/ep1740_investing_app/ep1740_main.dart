0656f2e1679d5e0fb47f8533f08dff16c941f16e
JAICHANGPARK <aristojeff@gmail.com>
2023-12-25 09:47:30 +0900

    Update ep1740_main.dart

--- lib/ep1740_investing_app/ep1740_main.dart
+++ lib/ep1740_investing_app/ep1740_main.dart
@@ -1,2 +1,14 @@
 import 'package:flutter/material.dart';
 
+void main() {
+  
+}
+
+class InvestingApp extends StatelessWidget {
+  const InvestingApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp();
+  }
+}
