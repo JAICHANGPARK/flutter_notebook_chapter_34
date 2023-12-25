15af851f8bd291fecab2683084ca81bbe83150c3
JAICHANGPARK <aristojeff@gmail.com>
2023-12-25 09:49:43 +0900

    Create investring_detail_page.dart

---
+++ lib/ep1740_investing_app/views/investring_detail_page.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+
+class InvestingDetailPage extends StatefulWidget {
+  const InvestingDetailPage({super.key});
+
+  @override
+  State<InvestingDetailPage> createState() => _InvestingDetailPageState();
+}
+
+class _InvestingDetailPageState extends State<InvestingDetailPage> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
