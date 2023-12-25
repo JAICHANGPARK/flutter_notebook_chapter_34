c760bf60e3fc9d9a8c8f46cbc7070fce94f66210
JAICHANGPARK <aristojeff@gmail.com>
2023-12-25 09:51:55 +0900

    Create investing_home_page.dart

---
+++ lib/ep1740_investing_app/views/investing_home_page.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+
+class InvestingHomePage extends StatefulWidget {
+  const InvestingHomePage({super.key});
+
+  @override
+  State<InvestingHomePage> createState() => _InvestingHomePageState();
+}
+
+class _InvestingHomePageState extends State<InvestingHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
