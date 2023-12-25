c211d7d4c9cd3156bf73c7a9b4e1771f25ffde31
JAICHANGPARK <aristojeff@gmail.com>
2023-12-25 09:58:40 +0900

    Update investring_detail_page.dart

--- lib/ep1740_investing_app/views/investring_detail_page.dart
+++ lib/ep1740_investing_app/views/investring_detail_page.dart
@@ -13,7 +13,41 @@ class _InvestingDetailPageState extends State<InvestingDetailPage> {
     return Scaffold(
       backgroundColor: Color.fromRGBO(23, 23, 23, 1),
       appBar: AppBar(
-        
+        backgroundColor: Color.fromRGBO(23, 23, 23, 1),
+        foregroundColor: Colors.grey,
+        centerTitle: true,
+        title: Text("Unknown"),
+      ),
+      body: Column(
+        children: [
+          Container(
+            height: 400,
+            child: Placeholder(),
+          ),
+          Row(
+            children: [
+              Text("About"),
+              OutlinedButton.icon(
+                onPressed: () {},
+                icon: Icon(Icons.check),
+                label: Text("Watching"),
+              ),
+            ],
+          ),
+          Text(
+            "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. ",
+            style: TextStyle(
+              color: Colors.grey,
+
+            ),
+            maxLines: 2,
+            overflow: TextOverflow.clip,
+          ),
+          Container(
+            height: 240,
+            color: Colors.white,
+          )
+        ],
       ),
     );
   }
