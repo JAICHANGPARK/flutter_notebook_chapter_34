7313321e2d9ec95235418afc0bab8a84890b3267
JAICHANGPARK <aristojeff@gmail.com>
2023-12-31 11:57:39 +0900

    Create booking_detail_page.dart

---
+++ lib/ep1743_booking_app/views/booking_detail_page.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+
+class BookingDetailPage extends StatefulWidget {
+  const BookingDetailPage({super.key});
+
+  @override
+  State<BookingDetailPage> createState() => _BookingDetailPageState();
+}
+
+class _BookingDetailPageState extends State<BookingDetailPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
